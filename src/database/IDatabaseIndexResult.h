1a90b265ce6c7d0db626b7500fb1b2107c752d2e
Vinokurov Kirill <vinokurov@elewise.com>
2017-12-15 12:26:58 +0400

    feature/database-index

--- src/database/IDatabaseIndexResult.h
+++ src/database/IDatabaseIndexResult.h
@@ -1,11 +1,25 @@
 #ifndef IDATABASEINDEXRESULT_H
 #define IDATABASEINDEXRESULT_H
 
+#include <QString>
 
 class IDatabaseIndexResult
 {
 public:
     IDatabaseIndexResult();
+
+    QString filename() { return _filename; }
+    qint64 offset() { return _offset; }
+    int size() { return _size; }
+
+    void setFilename(QString value) { _filename = value; }
+    void setOffset(qint64 value) { _offset = value; }
+    void setSize(int value) { _size = value; }
+
+private:
+    QString _filename;
+    qint64 _offset;
+    int _size;
 };
 
 #endif // IDATABASEINDEXRESULT_H
