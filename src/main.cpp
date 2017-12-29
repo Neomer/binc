0a26eb2dc84088a68e93b3d09d1b6f52cd68c459
Vinokurov Kirill <vinokurov@elewise.com>
2017-12-29 11:59:27 +0400

    Dev из файла main.cpp убран весь мусор

--- src/main.cpp
+++ src/main.cpp
@@ -24,56 +24,6 @@ int main(int argc, char ** argv)
     }
     Context::Instance().load();
 
-    DatabaseDataFileRecord rec;
-/*
-    rec.setData("|dfgdsfgdf");
-    rec.setBlockNumber(3464);
-
-    DatabaseDataFile file;
-    file.setFileName("test.data");
-    file.open(QIODevice::ReadWrite);
-    file.write(&rec);
-    file.close();
-*/
-    /*
-    QDateTime start = QDateTime::currentDateTime();
-    for (int i = 0; i < 100; i++)
-    {
-        int n = rand();
-        rec.setGuid(Guid::randomGuid());
-        rec.setBlockNumber(n);
-        rec.setData("test data");
-        Context::Instance().database()->write(rec.guid(), &rec);
-    }
-    qDebug() << "Elapsed:" << start.msecsTo(QDateTime::currentDateTime()) * 0.001;
-    */
-
-    quint16 port = 0;
-    try
-    {
-        PortMapping::tryMapPort(16787, &port);
-    }
-    catch (NetDataStreamException &ex)
-    {
-        qDebug() << ex.what();
-    }
-
-    try
-    {
-        HTTPResponse resp = HTTPClient::get(QUrl("http://yandex.ru"));
-        if (resp.status() != 200)
-        {
-            qDebug() << resp.statusMessage();
-        }
-    }
-    catch (HTTPParsingException &ex)
-    {
-
-        qDebug() << "Error: line" << ex.line() << ex.what();
-    }
-
-
-    //SSDPProvider::registerPort(1567);
     int ret = a.exec();
 
     Context::Instance().unload();
